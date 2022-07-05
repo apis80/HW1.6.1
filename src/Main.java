9307d37e814e76ea678701f23a2ee31d0a3c1102
apis8 <apis80@mail.ru>
2022-07-05 19:56:06 +0100

    Первый комит

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Ivan ";
+        String secondName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + secondName;
+        System.out.println(" ФИО сотрудника - " + fullName);
+        // Задача 2
+        String bigName = fullName.replace("Ivanov Ivan Ivanovich", " IVANOV IVAN IVANOVICH");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + bigName);
+        // Задача 3
+        String fullName1 = "Иванов Семён Семёнович";
+        String fullName2 = fullName1.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName2);
 
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
 
     }
 }
